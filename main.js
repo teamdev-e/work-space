86c2c297d10c6edd0335c81b70d239e1612d1c5d
lisahamada <tousokutyokusenundo@gmail.com>
2023-08-05 20:35:23 +0900

    revised-3

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let currentPlayer = 'X';
-        const cells = document.querySelectorAll('.cell');
+    const cells = document.querySelectorAll('.cell');
 
-        function makeMove(cell) {
-            if (!cell.classList.contains('X') && !cell.classList.contains('O')) {
-                cell.classList.add(currentPlayer);
+    function makeMove(cell) {
+        if (!cell.classList.contains('X') && !cell.classList.contains('O')) {
+            cell.classList.add(currentPlayer);
 
                 if (checkWin()) {
                     setTimeout(() => {
@@ -20,19 +20,38 @@ let currentPlayer = 'X';
                 }
             }
         }
+        
+    function checkWin() {
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // 横の勝利条件
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // 縦の勝利条件
+            [0, 4, 8], [2, 4, 6] // 斜めの勝利条件
+        ];
 
-        function checkWin() {
-            // 勝利条件をチェックするロジックを実装する
+        for (const pattern of winPatterns) {
+            const [a, b, c] = pattern;
+            if (cells[a].classList.contains(currentPlayer) &&
+                cells[b].classList.contains(currentPlayer) &&
+                cells[c].classList.contains(currentPlayer)) {
+                return true;
+            }
         }
 
+        return false;
+    }
 
-        function checkDraw() {
-            // 引き分け条件をチェックするロジックを実装する
+    function checkDraw() {
+        for (const cell of cells) {
+            if (!cell.classList.contains('X') && !cell.classList.contains('O')) {
+                return false; 
+            }
         }
+        return true; 
+    }
 
-        function resetBoard() {
-            cells.forEach(cell => {
-                cell.classList.remove('X', 'O');
-            });
-            currentPlayer = 'X';
-        }
+    function resetBoard() {
+        cells.forEach(cell => {
+            cell.classList.remove('X', 'O');
+        });
+        currentPlayer = 'X';
+    }
