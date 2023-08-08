29e3c55c2f198c0833ee4a1d852e200f374f6d3a
pe5 <hatomame930@gmail.com>
2023-08-08 21:06:48 +0900

    add_reset_board

--- main.js
+++ main.js
@@ -1,98 +1,129 @@
 let currentPlayer = 'X';
-    const cells = document.querySelectorAll('.cell');
-    const gameSelector = document.getElementById('gameSelector');
-    let savedGames = [];
-    function makeMove(cell) {
+const cells = document.querySelectorAll('.cell');
+const gameSelector = document.getElementById('gameSelector');
+let savedGames = [];
+
+const gameModeForm = document.getElementById('gameModeForm');
+let isComputerPlayer = false;
+
+gameModeForm.addEventListener('change', function() {
+    const selectedMode = gameModeForm.elements['mode'].value;
+    isComputerPlayer = (selectedMode === '1');
+    resetBoard();
+});
+
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        makeMove(cell);
+    });
+});
+
+function makeMove(cell) {
+    if (!cell.classList.contains('X') && !cell.classList.contains('O')) {
+        cell.classList.add(currentPlayer);
+            if (checkWin()) {
+                    alert(currentPlayer + 'の勝利！');
+                    resetBoard();
+                    return
+            }
+            } else if (checkDraw()) {
+                    alert('引き分け');
+                    resetBoard();
+                    return;
+            } else {
+                currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+                if (isComputerPlayer && currentPlayer === 'O') {
+                    makeComputerMove();
+            }
+        }
+    }
+    
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // 横の勝利条件
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // 縦の勝利条件
+        [0, 4, 8], [2, 4, 6] // 斜めの勝利条件
+    ];
+
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (cells[a].classList.contains(currentPlayer) &&
+            cells[b].classList.contains(currentPlayer) &&
+            cells[c].classList.contains(currentPlayer)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function checkDraw() {
+    for (const cell of cells) {
         if (!cell.classList.contains('X') && !cell.classList.contains('O')) {
-            cell.classList.add(currentPlayer);
-
-                if (checkWin()) {
-                    setTimeout(() => {
-                        alert(currentPlayer + 'の勝利！');
-                        resetBoard();
-                    }, 100);
-                } else if (checkDraw()) {
-                    setTimeout(() => {
-                        alert('引き分け');
-                        resetBoard();
-                    }, 100);
-                } else {
-                    currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
-                }
-            }
-        }
-        
-    function checkWin() {
-        const winPatterns = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // 横の勝利条件
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // 縦の勝利条件
-            [0, 4, 8], [2, 4, 6] // 斜めの勝利条件
-        ];
-
-        for (const pattern of winPatterns) {
-            const [a, b, c] = pattern;
-            if (cells[a].classList.contains(currentPlayer) &&
-                cells[b].classList.contains(currentPlayer) &&
-                cells[c].classList.contains(currentPlayer)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    function checkDraw() {
-        for (const cell of cells) {
-            if (!cell.classList.contains('X') && !cell.classList.contains('O')) {
-                return false; 
-            }
-        }
-        return true; 
-    }
-
-    function resetBoard() {
-        cells.forEach(cell => {
-            cell.classList.remove('X', 'O');
-        });
-        currentPlayer = 'X';
-    }
-
-    
-    function saveGame() {
-        const gameData = {
-            currentPlayer: currentPlayer,
-            cellClasses: Array.from(cells).map(cell => cell.className)
-        };
-    
-        savedGames.push(gameData);
-        if (savedGames.length > 5) {
-            savedGames.shift();
-        }
-    
-        updateGameSelector();
-    
-        localStorage.setItem('ticTacToeSavedGames', JSON.stringify(savedGames));
-    }
-    
-
-    function loadGame() {
-        const selectedIndex = gameSelector.value;
-        if (savedGames[selectedIndex]) {
-            const gameData = savedGames[selectedIndex];
-            currentPlayer = gameData.currentPlayer;
-            for (let i = 0; i < cells.length; i++) {
-                cells[i].className = gameData.cellClasses[i];
-            }
+            return false; 
         }
     }
+    return true; 
+}
+
+function resetBoard() {
+    cells.forEach(cell => {
+        cell.classList.remove('X', 'O');
+    });
+    currentPlayer = 'X';
+    if (isComputerPlayer && currentPlayer === 'O') {
+        makeComputerMove();
+    }
+}
+
+
+function makeComputerMove() {
+    const emptyCells = Array.from(cells).filter(cell => !cell.classList.contains('X') && !cell.classList.contains('O'));
     
-    function updateGameSelector() {
-        gameSelector.innerHTML = '';
-        for (let i = savedGames.length - 1; i >= 0; i--) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.text = 'Game ' + (savedGames.length - i);
-        gameSelector.appendChild(option);
+    if (emptyCells.length > 0) {
+        const randomIndex = Math.floor(Math.random() * emptyCells.length);
+        const selectedCell = emptyCells[randomIndex];
+        makeMove(selectedCell);
+    }
+}
+
+
+function saveGame() {
+    const gameData = {
+        currentPlayer: currentPlayer,
+        cellClasses: Array.from(cells).map(cell => cell.className)
+    };
+
+    savedGames.push(gameData);
+    if (savedGames.length > 5) {
+        savedGames.shift();
+    }
+
+    updateGameSelector();
+
+    localStorage.setItem('ticTacToeSavedGames', JSON.stringify(savedGames));
+}
+
+
+function loadGame() {
+    const selectedIndex = gameSelector.value;
+    if (savedGames[selectedIndex]) {
+        const gameData = savedGames[selectedIndex];
+        currentPlayer = gameData.currentPlayer;
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].className = gameData.cellClasses[i];
         }
-        }
-    
+    }
+}
+
+function updateGameSelector() {
+    gameSelector.innerHTML = '';
+    for (let i = savedGames.length - 1; i >= 0; i--) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.text = 'Game ' + (savedGames.length - i);
+    gameSelector.appendChild(option);
+    }
+}
+
+
